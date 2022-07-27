82cdfdc6741f4f1e2126e54621d3735632b54f37
dansadmin@homail.com <dansadmin@homail.com>
2022-07-27 15:51:35 +0200

    hash examples for tables

--- code/18-maps/05-hash01/main.go
+++ code/18-maps/05-hash01/main.go
@@ -4,40 +4,34 @@ import (
 	"bufio"
 	"fmt"
 	"net/http"
-	"os"
 )
 
 func main() {
-
 	resp, err := http.Get("https://raw.githubusercontent.com/AllenDowney/OlinPyShop/master/wordsEn.txt")
 	if err != nil {
 		fmt.Println(err)
 	}
 	//close response body after main function
 	defer resp.Body.Close()
-	words := make(map[string]string)
 
 	//scan the page
 	sc := bufio.NewScanner(resp.Body)
 	//set the split function for scanning operation
 	sc.Split(bufio.ScanLines)
 
+	//create slice to hold scanners
+
+	buckets := make([]int, 200)
+
 	for sc.Scan() {
-		words[sc.Text()] = ""
-
-	}
-	if err := sc.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading standart input:", err)
+		n := HasBucket(sc.Text())
+		buckets[n]++
 	}
 
-	i := 0
-	// prints only 200 words and not entire stuff
-	for w, _ := range words {
-		fmt.Println(w)
-		if i == 200 {
-			break
-		}
-		i++
-	}
+	fmt.Println(buckets[65:122])
 
+}
+
+func HasBucket(w string) int {
+	return int(w[0])
 }
