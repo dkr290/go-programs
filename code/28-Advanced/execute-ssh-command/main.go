f3a126f514d041e31f8ac53d18504a00126a90f1
Dansadmin <dansadmin@bankingcircle.com>
2022-08-25 17:03:08 +0200

    adding ssh-remote tool

---
+++ code/28-Advanced/execute-ssh-command/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"golang.org/x/crypto/ssh"
+)
+
+func main() {
+
+	argsWithProg := os.Args
+
+	if argsWithProg[1] == "help" {
+		hlp := `
+		 ssh-remote <hostname or ip address> <username> <path to the private key> "<command to execute/ or if multiple commands in brackets>"
+		`
+		fmt.Println(hlp)
+		return
+
+	}
+	if len(argsWithProg) != 5 {
+		fmt.Println("ssh-remote <hostname or ip address> <username> <path to the private key>", "<command to execute/ or if multiple commands in brackets>")
+
+		return
+	} else {
+		SshAndRunCommand(argsWithProg)
+	}
+
+}
+
+func SshAndRunCommand(args []string) error {
+
+	key, err := ioutil.ReadFile(args[3])
+	if err != nil {
+		log.Fatalf("unable to read private key: %v", err)
+	}
+
+	// Create the Signer for this private key.
+	signer, err := ssh.ParsePrivateKey(key)
+	if err != nil {
+		log.Fatalf("unable to parse private key: %v", err)
+	}
+
+	config := &ssh.ClientConfig{
+		User: args[2],
+		Auth: []ssh.AuthMethod{
+			ssh.PublicKeys(signer),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+	server := args[1] + ":22"
+
+	client, err := ssh.Dial("tcp", server, config)
+	if err != nil {
+		log.Fatal("Failed to dial: ", err)
+	}
+	defer client.Close()
+	session, err := client.NewSession()
+	if err != nil {
+		log.Fatal("Failed to create session: ", err)
+	}
+	defer session.Close()
+
+	// Once a Session is created, you can execute a single command on
+	// the remote side using the Run method.
+	var b bytes.Buffer
+	session.Stdout = &b
+	if err := session.Run(args[4]); err != nil {
+		log.Fatal("Failed to run: " + err.Error())
+	}
+	fmt.Println(b.String())
+	return nil
+}
